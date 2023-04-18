b522e23d1a4e7ba4e332911d875625ee7e459d66
Leon <leonhess2006@web.de>
2023-04-18 23:42:35 +0200

    auth finished

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -42,3 +42,89 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+  const loginEmailPassword = async () => {
+	const loginEmail = document.getElementById("txtEmail").value
+	const loginPassword = document.getElementById("txtPassword").value
+
+	try {
+	   await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+	}
+	catch(error) {
+	   console.log(`There was an error: ${error}`)
+	   showLoginError(error)
+	}
+	}
+
+  // Create new account using email/password
+  const createAccount = async () => {
+	const email = document.getElementById("txtEmail").value
+	const password = document.getElementById("txtPassword").value
+  
+	try {
+	  await createUserWithEmailAndPassword(auth, email, password)
+	}
+	catch(error) {
+	  console.log(`There was an error: ${error}`)
+	  showLoginError(error)
+	} 
+	}
+
+  export const hideLoginError = () => {
+	document.getElementById("divLoginError").style.display = 'none'
+	document.getElementById("lblLoginErrorMessage").innerHTML = ''
+  }
+  hideLoginError();
+
+  export const showLoginError = (error) => {
+	document.getElementById("divLoginError").style.display = 'block'    
+	if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+	  document.getElementById("lblLoginErrorMessage").innerHTML = `Wrong password. Try again.`
+	}
+	else {
+	  document.getElementById("lblLoginErrorMessage").innerHTML = `Error: ${error.message}`      
+	}
+	}
+
+ export const showLoginForm = () => {
+	document.getElementById("login").style.display = 'block'
+	document.getElementById("app").style.display = 'none'
+  }
+  
+ export const showApp = () => {
+	document.getElementById("login").style.display = 'none'
+	document.getElementById("app").style.display = 'block'
+  }
+
+  // Log out
+ const logout = async () => {
+	await signOut(auth);
+  }
+
+  // Monitor auth state
+ const monitorAuthState = async () => {
+	onAuthStateChanged(auth, user => {
+	  if (user) {
+		console.log(user)
+		showApp()
+		showLoginState(user)
+		hideLoginError()
+	  }
+	  else {
+		showLoginForm()
+		document.getElementById("lblAuthState").innerHTML = `You're not logged in.`
+	  }
+	})
+  }
+
+
+ export const showLoginState = (user) => {
+	//document.getElementById("lblAuthState").innerHTML = `You're logged in as ${user.displayName} (uid: ${user.uid}, email: ${user.email}) `
+	document.getElementById("lblAuthState").innerHTML = `You're logged in with the Email: ${user.email} `
+  }
+
+  document.getElementById("btnLogin").addEventListener("click", loginEmailPassword) 
+  document.getElementById("btnSignup").addEventListener("click", createAccount)
+  document.getElementById("btnLogout").addEventListener("click", logout)
+  const auth = getAuth(app);
+  monitorAuthState();
